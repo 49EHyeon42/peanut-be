2d4474bf0570dba4e621535d5ef1427a6a5aa1ce
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-06-29 14:54:50 +0900

    :sparkles: init project

---
+++ src/main/java/com/struc9ture/peanut/PeanutApplication.java
@@ -0,0 +1,13 @@
+package com.struc9ture.peanut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeanutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeanutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/struc9ture/peanut/PeanutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.struc9ture.peanut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeanutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
